250c9d1ae73a82c79ad10fc1360b070767710fa6
David Parga Torrente <david@Davids-MacBook-Pro.local>
2026-02-21 21:24:59 +0200

    Update RAW_PATH to INE camping dataset

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 ROOT = Path(__file__).resolve().parent.parent
 
 # Change these paths to point to your data files
-RAW_PATH = ROOT / "data" / "raw" / "raw_dataset.csv"
+RAW_PATH = ROOT / "data" / "raw" / "2064.csv"
 OUT_PATH = ROOT / "data" / "processed" / "clean_dataset.csv"
