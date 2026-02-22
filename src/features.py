54c95f818d06cd30d5cdfe0508ae500c2b7bf2b4
David Parga Torrente <david@Davids-MacBook-Pro.local>
2026-02-22 17:05:07 +0200

    Solved errors on features.py

--- src/features.py
+++ src/features.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 ### FEATURES PARA ANÁLISIS ESTRÁTEGICO DEL SECTOR ###
-#--------------------------------------------------
-#   Incluimos nuevas columnas a nuestro data frame para enriquecerlo con más variables
-#--------------------------------------------------
+# --------------------------------------------------
+#   Incluimos nuevas columnas a nuestro data frame
+#   para enriquecerlo con más variables
+# --------------------------------------------------
 
 def build_features(df: pd.DataFrame) -> pd.DataFrame:
 
@@ -11,15 +12,30 @@ def build_features(df: pd.DataFrame) -> pd.DataFrame:
     # Intensidad laboral
     #   Tamaño medio de plantilla
     # --------------------------------------------
-    df["empleados_por_establecimiento"] = df["Personal"] / df["Establecimientos"].replace(0, pd.NA)
-    df["empleados_por_parcela"] = df["Personal"] / df["Parcelas"].replace(0, pd.NA)
+    df["empleados_por_establecimiento"] = (
+        df["Personal"] / df["Establecimientos"].replace(0, pd.NA)
+    )
+
+    df["empleados_por_parcela"] = (
+        df["Personal"] / df["Parcelas"].replace(0, pd.NA)
+    )
 
     # --------------------------------------------
     # Tamaño medio del camping
     # --------------------------------------------
-    df["parcelas_por_establecimiento"] = df["Parcelas"] / df["Establecimientos"].replace(0, pd.NA)
+    df["parcelas_por_establecimiento"] = (
+        df["Parcelas"] / df["Establecimientos"].replace(0, pd.NA)
+    )
 
- 
+    # --------------------------------------------
+    # Ocupación real mensual
+    #   Ajustamos por número de días del mes
+    # --------------------------------------------
+    df["dias_mes"] = df["date"].dt.days_in_month
 
+    df["ocupacion_real"] = (
+        df["Parcelas_Ocupadas"] /
+        (df["Parcelas"] * df["dias_mes"])
+    )
 
     return df
