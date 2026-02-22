e4b0328ac0994cf059c21da1a91ceb0517dacf91
David Parga Torrente <david@Davids-MacBook-Pro.local>
2026-02-22 13:20:45 +0200

    Add strategic business features for camping sector analysis

--- src/features.py
+++ src/features.py
@@ -1,5 +1,25 @@
 import pandas as pd
 
+### FEATURES PARA ANÁLISIS ESTRÁTEGICO DEL SECTOR ###
+#--------------------------------------------------
+#   Incluimos nuevas columnas a nuestro data frame para enriquecerlo con más variables
+#--------------------------------------------------
 
 def build_features(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+
+    # --------------------------------------------
+    # Intensidad laboral
+    #   Tamaño medio de plantilla
+    # --------------------------------------------
+    df["empleados_por_establecimiento"] = df["Personal"] / df["Establecimientos"].replace(0, pd.NA)
+    df["empleados_por_parcela"] = df["Personal"] / df["Parcelas"].replace(0, pd.NA)
+
+    # --------------------------------------------
+    # Tamaño medio del camping
+    # --------------------------------------------
+    df["parcelas_por_establecimiento"] = df["Parcelas"] / df["Establecimientos"].replace(0, pd.NA)
+
+ 
+
+
+    return df
